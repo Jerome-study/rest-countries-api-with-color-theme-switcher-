e7584e5ce48e7cdcda7411323a1ecd7fdf126822
jerome <jerome.studypurpose@gmail.com>
2023-11-25 12:01:34 +0800

    fix

--- src/components/Search.js
+++ src/components/Search.js
@@ -6,10 +6,10 @@ import { useContext, useEffect, useState } from 'react';
 import { useFetch } from '../hooks/useFetch';
 import { useNavigate } from 'react-router-dom';
 export const Search = () => {
-    const { filterRegion, setFilterRegion, regionName, setRegionName, setFilterOn, isLightModeOn } = useContext(AppContext);
+    const { setFilterRegion, regionName, setRegionName, setFilterOn, isLightModeOn } = useContext(AppContext);
     const [searchFilterOn, setSearchFilterOn] = useState(false);
     const [searchBar, setSearchBar] = useState(null)
-    const [data,loading,error] = useFetch("../data.json");
+    const [data,loading,error] = useFetch("data.json");
     const [regions, setRegions] = useState([]);
     const [searchResult, setSearchResult] = useState(null)
     const [loadingResult, setLoadingResult] = useState(true);
@@ -77,7 +77,7 @@ export const Search = () => {
             <div className="container d-flex justify-content-between  flex-lg-row flex-column flex-md-column flex-sm-column mt-2 mb-4 mb-md-3 mb-lg-3  ">
                 <div  className="form-group has-search w-100 mb-3"  style={{maxWidth: "25rem"}}>
                     <span className="fa  fa-search form-control-feedback"></span>
-                    <input type="text" className={isLightModeOn?"shadow-sm py-3 rounded form-control mb-1":"shadow-sm py-3 rounded form-control dark-mode-1 mb-1"}   placeholder="Search for a country..."  onChange={(e) =>{setSearchBar(e.target.value), setSearchFilterOn(true), searchCountry(e.target.value)} }    />
+                    <input type="text" className={isLightModeOn?"shadow-sm py-3 rounded form-control mb-1":"shadow-sm py-3 rounded form-control dark-mode-1 mb-1"}   placeholder="Search for a country..."  onChange={(e) =>{setSearchBar(e.target.value); setSearchFilterOn(true); searchCountry(e.target.value);} }    />
                     <ul className={isLightModeOn?searchFilterOn?"shadow-sm dropdown-menu w-75 display-block":"shadow-sm dropdown-menu w-75":searchFilterOn?" dark-mode-1 shadow-sm dropdown-menu w-75 display-block":" dark-mode-1 shadow-sm dropdown-menu w-75"} style={{maxWidth:"25rem"}}>
                         {loadingResult?<h1>Loading...</h1>:searchResult?.length > 0? searchResult?.map((result,index) => {
                             return <li key={index} onClick={() => selectCountry(result)}><a className={isLightModeOn?"dropdown-item":"dropdown-item dark-mode-1"} >{result.name}</a></li>
@@ -94,9 +94,9 @@ export const Search = () => {
                             <FontAwesomeIcon icon={faChevronDown} />
                     </button>
                     <ul className={isLightModeOn?"shadow-sm dropdown-menu w-100":"shadow-sm dropdown-menu w-100 dark-mode-1"}>
-                    <li><a className={isLightModeOn?"dropdown-item":"dropdown-item dark-mode-1"} href="#"  onClick={() => {setFilterRegion(null), setRegionName("All"), setFilterOn(false)}}>All</a></li>
+                    <li><a className={isLightModeOn?"dropdown-item":"dropdown-item dark-mode-1"} href="#"  onClick={() => {setFilterRegion(null); setRegionName("All"); setFilterOn(false);}}>All</a></li>
                         {regions?.map((region,key) => {
-                                return <li key={key}><a className={isLightModeOn?"dropdown-item":"dropdown-item dark-mode-1"} href="#"  onClick={() => {setFilterRegion(region), setRegionName(region), setFilterOn(true)}}>{region}</a></li>
+                                return <li key={key}><a className={isLightModeOn?"dropdown-item":"dropdown-item dark-mode-1"} href="#"  onClick={() => {setFilterRegion(region); setRegionName(region); setFilterOn(true);}}>{region}</a></li>
                         })}
                     </ul>
                 </div>
